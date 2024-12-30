fde2860b832741500266f0a04ea2c015d601c46c
pramodkhalkar <pramodkhalkar70@gmail.com>
2024-12-30 02:10:18 -0800

    Update and rename Name.java to WlcMsg.java

--- Name.java
+++
@@ -1,6 +0,0 @@
-public class Name {
-    public static void main(String[] args) {
-        // Replace "Your Name" with your desired name
-        System.out.println("Your Name");
-    }
-}
---
+++ WlcMsg.java
@@ -0,0 +1,5 @@
+WlcMsg {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the CI/CD Pipeline Practical!");
+    }
+}
